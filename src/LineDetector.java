53bd6fc32ddf31f3d858393ce416788a14562f95
Ian Murphy <iano200@gmail.com>
2013-11-23 13:18:34 +0000

    Initial Commit, started Assign 2, whoop

---
+++ src/LineDetector.java
@@ -0,0 +1,17 @@
+import edu.princeton.cs.introcs.In;
+
+public class LineDetector {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        In fileIn = new In(args[0]);
+        int k = fileIn.readInt();
+
+
+
+	}
+
+}
---
+++ src/Point.java
@@ -0,0 +1,88 @@
+import edu.princeton.cs.introcs.StdDraw;
+
+import java.util.Comparator;
+
+
+public class Point implements Comparable<Point> {
+
+    // compare points by slope
+    public final Comparator<Point> SLOPE_ORDER = new BySlope();       // YOUR DEFINITION HERE
+
+
+
+    private final int x;                              // x coordinate
+    private final int y;                              // y coordinate
+
+    // create the point (x, y)
+    public Point(int x, int y) {
+        /* DO NOT MODIFY */
+        this.x = x;
+        this.y = y;
+    }
+
+    // plot this point to standard drawing
+    public void draw() {
+        /* DO NOT MODIFY */
+        StdDraw.point(x, y);
+    }
+
+    // draw line between this point and that point to standard drawing
+    public void drawTo(Point that) {
+        /* DO NOT MODIFY */
+        StdDraw.line(this.x, this.y, that.x, that.y);
+    }
+
+    // slope between this point and that point
+    public double slopeTo(Point that) {
+        if (this.compareTo(that) == 0) return Double.NEGATIVE_INFINITY;
+        if (this.x == that.x) return Double.POSITIVE_INFINITY;
+        if (this.y == that.y) return 0d;
+
+        double dx = that.x - this.x, dy = that.y - this.x;
+
+        return dy/dx;
+    }
+
+    // is this point lexicographically smaller than that one?
+    // comparing y-coordinates and breaking ties by x-coordinates
+    public int compareTo(Point that) {
+        /* YOUR CODE HERE */
+        if ((this.y < that.y)) return -1;
+
+        if (this.y == that.y) {
+            if (this.x == that.x) return 0;
+            if (this.x < that.x) return -1;
+        }
+        return +1;
+    }
+
+    // return string representation of this point
+    public String toString() {
+        /* DO NOT MODIFY */
+        return "(" + x + ", " + y + ")";
+    }
+
+    public class BySlope implements Comparator<Point> {
+        @Override
+        public int compare(Point p1, Point p2) {
+            double slope1 = Point.this.slopeTo(p1), slope2 = Point.this.slopeTo(p2);
+
+            if (slope1 > slope2) return +1;
+            else if (slope1 < slope2) return -1;
+            else return 0;
+        }
+    }
+
+    // unit test
+    public static void main(String[] args) {
+        /* YOUR CODE HERE */
+    }
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+}
